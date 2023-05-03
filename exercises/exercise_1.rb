e90847cdece9ffab9f0573981c9f0310d37cbb07
tehi_kim <k.tehi21@gmail.com>
2023-05-03 17:04:34 +0000

    create 3 stores and counting total stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,16 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+total_count = Store.count
+puts "Total number of stores: #{total_count}"
+
+# create_table :stores do |t|
+#   t.column :name, :string
+#   t.column :annual_revenue, :integer
+#   t.column :mens_apparel, :boolean
+#   t.column :womens_apparel, :boolean
+#   t.timestamps null: false
