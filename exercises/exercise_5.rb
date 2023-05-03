bde34422fb77e7af680872044d1b1c29ecd150b7
tehi_kim <k.tehi21@gmail.com>
2023-05-03 17:39:12 +0000

    store's total, ave revenue/get the stores that over 1M revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue store: #{@total_revenue}"
+
+@ave_revenue = Store.average(:annual_revenue)
+puts "Average revenue store: #{@ave_revenue}"
+
+@high_store = Store.where("annual_revenue > ?", 1000000).count
+puts "High revenue store: #{@high_store}"
+
