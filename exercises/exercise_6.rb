88650d3eae191c35ae14e49e02f0dd3ca641c2c9
tehi_kim <k.tehi21@gmail.com>
2023-05-03 18:52:43 +0000

    store, employee inherait ActiveRecord::Base/ then add employee under store1

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -17,4 +17,16 @@ class Employee < ActiveRecord::Base
   belongs_to :store
 end
 
-@store1.employees.create(store_id: 1, first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Strawberry", last_name: "Jam", hourly_rate: 20)
+@store1.employees.create(first_name: "Orange", last_name: "Juice", hourly_rate: 30)
+@store1.employees.create(first_name: "Hong", last_name: "Sam", hourly_rate: 23)
+@store1.employees.create(first_name: "Star", last_name: "Bucks", hourly_rate: 34)
+@store1.employees.create(first_name: "Kirkland", last_name: "Costco", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Dane", last_name: "Libby", hourly_rate: 50)
+@store2.employees.create(first_name: "Tex", last_name: "Kimmy", hourly_rate: 25)
+@store2.employees.create(first_name: "Easter", last_name: "Elliot", hourly_rate: 32)
+@store2.employees.create(first_name: "Lilibet", last_name: "Madelaine", hourly_rate: 33)
+@store2.employees.create(first_name: "Columbine", last_name: "Colin", hourly_rate: 31)
+@store2.employees.create(first_name: "Lawan", last_name: "Mongkut", hourly_rate: 50)
