84d5caeba9ed18059fde3a73cd485d8aaf257ec4
tehi_kim <k.tehi21@gmail.com>
2023-05-03 19:56:51 +0000

    Validates each column. To intentionally fail to create a new storage, only the new storage name is imported from the user.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,39 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+class Store < ActiveRecord::Base
+  validate :store_must_has_at_least_one_of_mens_apparel_or_womens_apparel
+
+  def store_must_has_at_least_one_of_mens_apparel_or_womens_apparel
+    if !mens_apparel? && !womens_apparel?
+      errors.add(:mens_apparel, "either mens or womens apparel must be true")
+    end
+  end
+end
+
+puts "Please give me a store name:"
+@store_name = gets.chomp
+@store = Store.new(name: @store_name)
+
+if @store.save
+  puts "Store created successfully!"
+else
+  puts "Errors:"
+  @store.errors.full_messages.each do |message|
+    puts "#{message}"
+  end
+end
