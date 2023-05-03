efd6ad4f5d5821cbbe5754f87316f9de0fa78d9b
tehi_kim <k.tehi21@gmail.com>
2023-05-03 17:30:06 +0000

    add 3 more stores, selete only men_apparel store, and the store where carry womens apparel and less then 1M in anuual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(womens_apparel: false)
+@mens_stores.each do |m|
+  puts "Store name: #{m.name}, Annual revenue: #{m.annual_revenue}"
+end
+
+@womens_store_small = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+@womens_store_small.each do |w|
+  puts "Store name: #{w.name}, Annual revenue: #{w.annual_revenue}"
+end
