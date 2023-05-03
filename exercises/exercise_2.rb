0ca57d1d64a1fa8277d42134ae21f0ce6d27abc1
tehi_kim <k.tehi21@gmail.com>
2023-05-03 17:11:08 +0000

    find and define 2 stores, and change the first store's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.update(name: "Burnaby Renewal")
+puts "#{@store1} is the first store"
+
+
+@store2 = Store.find_by(id: 2)
+puts "#{@store2} is the second store"
