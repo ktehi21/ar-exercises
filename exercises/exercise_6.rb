268520ed27613931250174d0c7aeb6653c74f456
tehi_kim <k.tehi21@gmail.com>
2023-05-03 18:47:24 +0000

    store, employee inherait ActiveRecord::Base/ then add employee under store1

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base    
+  has_many :employees                   
+end
+class Employee < ActiveRecord::Base      
+  belongs_to :store
+end
+
+@store1.employees.create(store_id: 1, first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
